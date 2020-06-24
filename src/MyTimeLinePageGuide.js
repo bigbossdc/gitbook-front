66d6b3b5f39f7fd832a071613ce0ff73a33a5132
haloa19 <hj199514@gmail.com>
2020-06-24 14:45:56 +0900

    navi guide css

--- src/MyTimeLinePageGuide.js
+++ src/MyTimeLinePageGuide.js
@@ -24,9 +24,11 @@ class MyTimeLinePageGuide extends Component {
             <Link to={`/gitbook/upload/${this.props.groupno}`}>
              <p className="timeline-file-icon"><i class="fas fa-file-medical fa-6x"></i></p>
             </Link>
-            : <Link to="/gitbook/upload">
+            : (this.props.userid === sessionStorage.getItem("authUserId")) ?
+              <Link to="/gitbook/upload">
                 <p className="timeline-file-icon"><i className="fas fa-file-medical fa-6x"></i></p>
               </Link>
+              :<p style={{color:"#5b6160"}}><i class="fas fa-file-alt fa-6x"></i></p>
         }
 
       </div>
@@ -36,8 +38,9 @@ class MyTimeLinePageGuide extends Component {
         {
           (this.props.groupno && this.props.groupno !== undefined) ? 
             <p style={{fontFamily:"'Nanum Gothic', sans-serif", fontSize:"1.2em", color:"#606665"}}>팀원들에게 하고 싶은 이야기가 있나요? <br/> 아래 가이드를 보고 지금 업로드해보세요</p>
-            :<p style={{fontFamily:"'Nanum Gothic', sans-serif", fontSize:"1.2em", color:"#606665"}}>깃북에 처음 방문하셨나요? <br/> 아래 가이드를 보고 "{this.props.userid}"님의 이야기를 업로드해보세요</p>
-      
+            :(this.props.userid === sessionStorage.getItem("authUserId")) ? 
+              <p style={{fontFamily:"'Nanum Gothic', sans-serif", fontSize:"1.2em", color:"#606665"}}>깃북에 처음 방문하셨나요? <br/> 아래 가이드를 보고 "{this.props.userid}"님의 이야기를 업로드해보세요</p>
+              :<p><br/></p>
         }
       </div>
 
