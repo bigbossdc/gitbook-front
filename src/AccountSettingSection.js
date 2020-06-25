5980085a6ba4032fcd02504fd93c3a94e143df97
John Doe <johndoe@example.com>
2020-06-25 16:15:03 +0900

    20200625 final JeongSeok

--- src/AccountSettingSection.js
+++ src/AccountSettingSection.js
@@ -91,7 +91,6 @@ export default class AccountSettingSection extends Component {
 	}
 
 	render() {
-		console.log("called AccountSettingSection...");
 		return (
 			<Fragment>
 				{this.state.editable ? (
