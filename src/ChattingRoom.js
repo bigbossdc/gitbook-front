fe353c1ab72729b69c7cb1b8ddf6551fa9fcece2
yoonamgil <skaska5@naver.com>
2020-07-13 10:21:21 +0900

    end

--- src/ChattingRoom.js
+++ src/ChattingRoom.js
@@ -885,20 +885,13 @@ class ChattingRoom extends Component {
 		if (this.props.chatInfo && this.props.chatInfo) {
 			
 
-			
+
 			var scrollHeigth= window.jQuery(document.getElementsByClassName("conversation-container")).prop('scrollHeight')
 			window.jQuery(document.getElementsByClassName("conversation-container")).scrollTop(scrollHeigth);
 
-		
 			
 		}
-
-	
 	}
-
-
-
-
 
 }
 export default ChattingRoom;
